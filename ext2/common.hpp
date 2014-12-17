66c756c719027c77f44815abf102970fcf8f5a15
zschoche <github@zschoche.org>
2014-12-17 16:00:51 +0100

    Oops.

--- ext2/common.hpp
+++ ext2/common.hpp
@@ -3,8 +3,8 @@
 *	Author: Philipp Zschoche, https://zschoche.org
 *
 */
-#ifndef __EXT2_STUCTS_HPP__
-#define __EXT2_STUCTS_HPP__
+#ifndef __EXT2_COMMON_HPP__
+#define __EXT2_COMMON_HPP__
 
 namespace ext2 {
 
@@ -16,4 +16,4 @@ namespace detail {
 
 } /* namespace ext2 */
 
-#endif /* __EXT2_STUCTS_HPP__ */
+#endif /* __EXT2_COMMON_HPP__ */
