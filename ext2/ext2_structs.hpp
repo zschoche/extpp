0e0daddee5d48cda6851e7c37ab9774ad6b89cc9
Robin <robin.wloka@uni-oldenburg.de>
2014-12-19 16:40:11 +0100

    corrected "stucts" to "structs".

--- ext2/ext2_structs.hpp
+++ ext2/ext2_structs.hpp
@@ -3,8 +3,8 @@
 *	Author: Philipp Zschoche, https://zschoche.org
 *
 */
-#ifndef __EXT2_STUCTS_HPP__
-#define __EXT2_STUCTS_HPP__
+#ifndef __EXT2_STRUCTS_HPP__
+#define __EXT2_STRUCTS_HPP__
 
 namespace ext2 {
 namespace detail {
@@ -121,4 +121,4 @@ enum readonly_feature_flags{
 } /* namespace detail */
 } /* namespace ext2 */
 
-#endif /* __EXT2_STUCTS_HPP__ */
+#endif /* __EXT2_STRUCTS_HPP__ */
